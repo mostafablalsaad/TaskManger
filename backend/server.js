0f0515b5ca111fbc0145657607e61ff27577de60
mostafablalsaad <82916063+mostafablalsaad@users.noreply.github.com>
2025-10-09 02:39:11 +0300

    add the server export

--- backend/server.js
+++ backend/server.js
@@ -84,4 +84,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
-module.exports.handler = serverless(app);
+module.exports= serverless(app);
